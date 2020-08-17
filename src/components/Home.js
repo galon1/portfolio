4bc9a9c79366df90a0cb777cbadd7b150a0640cb
galon1 <gil.alon@strongsuit.co>
2020-08-16 21:18:03 -0400

    Added See Me button and added width to projects description

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import '../css/Home.css';
 import Navigation from './Navigation.js'
-import { Button, Tooltip } from 'antd';
+import { Button, Tooltip, Modal } from 'antd';
 import { LinkedinOutlined, GithubOutlined } from '@ant-design/icons';
 import Projects from './Projects.js';
+import mypicture from '../uploads/mypicture.JPG'
 
 class Home extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            modalVisible: false
+        }
+        this.openModal = this.openModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
+      }
+
+      openModal() {
+          this.setState({modalVisible: true});
+      }
+
+      closeModal() {
+        this.setState({modalVisible: false});
+    }
 
     render() {
         return (
@@ -26,18 +44,26 @@ class Home extends React.Component {
             </div>
             <h2 className="section-title" id="about">ABOUT ME</h2>
             <div className="wrapper">
-            {/* <Button > SEE ME! </Button> */}
             <p className="about-text"> 
             Hi! My name is Gil Alon and I am currently a junior studying computer science at Brown University.
                                 I am very interested in machine learning and cybersecurity and how using those technologies can better lives. I am very open to 
                                 most types of development. However, I need to work in a highly collaborative environment and I like
-                                to solve complex problems.</p>
+                                to solve complex problems.
+                                <Button onClick= {()=> this.openModal()} type="primary" size="small" className="see-me-button"> SEE ME! </Button>
+                                </p>
         <Button  type="primary" className="back-to-top-buttons" onClick={()=> document.documentElement.scrollTop = 0}> Back To Top</Button>
                                 <div className="border"></div>
            </div>
             <h2 className="section-title" id ="projects">PROJECTS <br /> </h2>
             <Projects />
             <Button  type="primary" className="back-to-top-buttons" onClick={()=> document.documentElement.scrollTop = 0}> Back To Top</Button>
+            <Modal
+            title="My Picture"
+            visible={this.state.modalVisible}
+            footer={null}
+            onCancel={this.closeModal}>
+            <img src={mypicture} className="me-picture" alt="" />
+            </Modal>
             </>
         )
     }
